70166a9922edde48903e8bca23bd8bc6de9f1154
clone <umarbektokyo@icloud.com>
2023-06-04 20:40:32 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from bridge import get_ip, get_data
+import os
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+
+@app.route('/', methods=["GET", "POST"])
+def trap():
+    data = get_data(get_ip())
+    return render_template('main.html', data=data)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
