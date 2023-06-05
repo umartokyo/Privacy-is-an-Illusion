1db00a814ded6380aaf0bf9572bf00119275dad6
clone <umarbektokyo@icloud.com>
2023-06-05 19:13:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from bridge import get_ip, get_data
-#import os
+import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret key"
@@ -11,5 +11,4 @@ def trap():
     return render_template('main.html', data=data)
 
 if __name__ == "__main__":
-    app.run()
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
